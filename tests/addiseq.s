a5b88cfe9ae6a0857663a9c0001ea77627cae561
Ethan Griesman <144555172+EthanGriesman@users.noreply.github.com>
2024-02-28 18:16:45 -0600

    Create addiseq.s

---
+++ tests/addiseq.s
@@ -0,0 +1,21 @@
+#
+# First part of the Lab 3 test program
+#
+
+# data section
+.data
+
+# code/instruction section
+.text
+addi  $1,  $0,  1 		# Place â1â? in $1
+addi  $2,  $0,  2		# Place â2â? in $2
+addi  $3,  $0,  3		# Place â3â? in $3
+addi  $4,  $0,  4		# Place â4â? in $4
+addi  $5,  $0,  5		# Place â5â? in $5
+addi  $6,  $0,  6		# Place â6â? in $6
+addi  $7,  $0,  7		# Place â7â? in $7
+addi  $8,  $0,  8		# Place â8â? in $8
+addi  $9,  $0,  9		# Place â9â? in $9
+addi  $10, $0,  10		# Place â10â? in $10
+
+halt
