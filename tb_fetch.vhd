0b82eb3bbda20b4f3520d842e77f434ac54814f7
Ethan Griesman <144555172+EthanGriesman@users.noreply.github.com>
2024-03-20 15:13:57 -0500

    Update tb_fetch.vhd

--- tb_fetch.vhd
+++ tb_fetch.vhd
@@ -18,7 +18,9 @@ use std.env.all;                -- For hierarchical/external signals
 use std.textio.all;             -- For basic I/O
 
 entity tb_fetch is
-  generic(gCLK_HPER   : time := 10 ns);   -- Generic for half of the clock cycle period
+  generic(
+    gCLK_HPER   : time := 10 ns  -- Generic for half of the clock cycle period
+  );
 end tb_fetch;
 
 architecture arch of tb_fetch is
@@ -27,25 +29,40 @@ architecture arch of tb_fetch is
   constant cCLK_PER : time := gCLK_HPER * 2;
   
   component fetch is 
-	port (
-		  CLK			: in std_logic;
-		  i_Input		: in std_logic_vector(31 downto 0);		--Input address into PC
-		  o_Out			: out std_logic_vector(31 downto 0));	--Output address from program counter
+    port (
+      iRST      : in std_logic;
+      iRSTVAL   : in std_logic_vector(31 downto 0);
+      iAddr     : in std_logic_vector(25 downto 0);
+      iSignExtendImm : in std_logic_vector(31 downto 0);
+      iBranch   : in std_logic;
+      iALUZero  : in std_logic;
+      iJump     : in std_logic;
+      oPC       : out std_logic_vector(31 downto 0);
+      oPCPlus4  : out std_logic_vector(31 downto 0)
+    );
   end component;
 
 -- Create signals for all of the inputs and outputs of the file that you are testing
-signal iCLK, reset : std_logic := '0';
+  signal iCLK, reset : std_logic := '0';
 
-signal s_Input		: std_logic_vector(31 downto 0);
-signal s_Output		: std_logic_vector(31 downto 0);
+  signal s_Rst, s_RstVal, s_Addr, s_SignExtendImm : std_logic_vector(31 downto 0);
+  signal s_Branch, s_ALUZero, s_Jump : std_logic;
+  signal s_PC, s_PCPlus4 : std_logic_vector(31 downto 0);
 
 begin 
 
-	DUT0: fetch
-		port map(
-				  CLK		=> iCLK,
-				  i_Input	=> s_Input,
-				  o_Out		=> s_Output);
+  DUT0: fetch
+    port map(
+      iRST      => s_Rst,
+      iRSTVAL   => s_RstVal,
+      iAddr     => s_Addr,
+      iSignExtendImm => s_SignExtendImm,
+      iBranch   => s_Branch,
+      iALUZero  => s_ALUZero,
+      iJump     => s_Jump,
+      oPC       => s_PC,
+      oPCPlus4  => s_PCPlus4
+    );
 				  
  --This first process is to setup the clock for the test bench
   P_CLK: process
@@ -56,32 +73,34 @@ begin
     wait for gCLK_HPER; -- after half a cycle, process begins evaluation again
   end process;
   
-			   
   -- This process resets the sequential components of the design.
   -- It is held to be 1 across both the negative and positive edges of the clock
   -- so it works regardless of whether the design uses synchronous (pos or neg edge)
   -- or asynchronous resets.
   P_RST: process
   begin
-  	reset <= '0';   
+    reset <= '0';   
     wait for gCLK_HPER/2;
-	reset <= '1';
+    reset <= '1';
     wait for gCLK_HPER*2;
-	reset <= '0';
-	wait;
+    reset <= '0';
+    wait;
   end process;
   
   -- Assign inputs for each test case.
   P_TEST_CASES: process
   begin
     wait for gCLK_HPER/2; -- for waveform clarity, NOT changing inputs on clk edges
-	
-  --Test case 1:
-  s_Input		<= x"000000a0";
-  wait for gCLK_HPER*2;
-  wait for gCLK_HPER*2;
+    
+    -- Test case 1:
+    s_Addr <= x"000000a0";
+    s_Rst <= '0';
+    s_RstVal <= (others => '0');
+    s_SignExtendImm <= (others => '0');
+    s_Branch <= '0';
+    s_ALUZero <= '0';
+    s_Jump <= '0';
+    wait for cCLK_PER; -- Wait for one clock cycle
   end process;
   
 end arch;
-
-	
