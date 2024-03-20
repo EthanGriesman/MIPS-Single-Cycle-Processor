9a188112b37483dbfaad040eb289749ac35262a9
Ethan Griesman <144555172+EthanGriesman@users.noreply.github.com>
2024-03-20 15:26:21 -0500

    Update controlModule.vhd

--- controlModule.vhd
+++ controlModule.vhd
@@ -1,75 +1,230 @@
--------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------------
 -- Ethan Griesman
--------------------------------------------------------------------------
+-- Department of Electrical and Computer Engineering
+-- Iowa State University
+-------------------------------------------------------------------------------------------------
 
 
 -- controlModule.vhd
--------------------------------------------------------------------------
--- DESCRIPTION: This file contains an implementation of the control unit
--- for the MIPS single cycle processor
---
---
--- NOTES:
--- 03/08/24 by JB::Design created.
--------------------------------------------------------------------------
--- library declaration
+-------------------------------------------------------------------------------------------------
+-- DESCRIPTION: This file contains an implementation of the control module of the MIPS processor
+-------------------------------------------------------------------------------------------------
+
 library IEEE;
 use IEEE.std_logic_1164.all;
--- entity
-entity controlModule is
-	port(i_opcode  	  	: in std_logic_vector(5 downto 0);
-	     i_funct	  	: in std_logic_vector(5 downto 0);
-	     o_Ctrl_Unt		: out std_logic_vector(14 downto 0));
+
+entity controlModule is  --Separate File for ALUControl?
+port(iOpcode    : in std_logic_vector(5 downto 0);
+     iFunct     : in std_logic_vector(5 downto 0);
+     oAl        : out std_logic;
+     oALUSrc    : out std_logic;
+     oALUOp     : out std_logic_vector(3 downto 0);
+     oMemtoReg  : out std_logic;
+     oMemWrite  : out std_logic;
+     oMemRead   : out std_logic;
+     oRegWrite  : out std_logic;
+     oRegDst    : out std_logic_vector(1 downto 0);
+     oJump      : out std_logic;
+     oBranch    : out std_logic;
+     oLb        : out std_logic;
+     oSignExtend: out std_logic;
+     oOverflowEn: out std_logic;
+     oHalt      : out std_logic);
 end controlModule;
 
--- architecture
-architecture dataflow of control_unit is
-signal s_RTYPE : std_logic_vector(14 downto 0);
+
+
+architecture dataflow of controlModule is
+
+signal s_aluOp1   : std_logic_vector(3 downto 0);
+signal s_aluOp2   : std_logic_vector(3 downto 0);
+
+signal s_rw1      : std_logic;
+signal s_rw2      : std_logic;
+
+signal s_Rds1     : std_logic_vector(1 downto 0);
+signal s_Rds2     : std_logic_vector(1 downto 0);
+
+signal s_j1       : std_logic;
+signal s_j2       : std_logic;
+
+signal s_se1      : std_logic;
+signal s_se2      : std_logic;
+
+signal s_ofe1     : std_logic;
+signal s_ofe2     : std_logic;
+
+--Uses spreadsheet to write select statements
+
+
 begin
 
-------------------------- FORMAT of o_Ctrl_Unt ----------------------------
--- " 14     13      12       11-8	      7	        6      5       4       3       2    1   0
--- " 0      0       0       0000          0         0      1       1       0       1    0"  0
--- " jr     jal     ALUSrc  ALUControl  MemtoReg  we_mem  we_reg  RegDst  PCSrc  SignExt j" Halt
---                                                                        Branch
----------------------------------------------------------------------------
+--ALUSrc--
+with opCode select
+     ALUSrc <= '1' when "001000",  -- addi
+               '1' when "001001",  -- addiu
+               '1' when "001100",  -- andi
+               '1' when "001111",  -- lui
+               '1' when "001110",  -- lw
+               '1' when "001101",  -- ori
+               '1' when "001010",  -- slti
+               '1' when "101000",  -- 
+               '1' when "101011",  -- sw
+               '1' when "011111",  -- lui
+               '1' when "100011",  -- lw
+               '0' when others;
 
-with i_funct select s_RTYPE <=
-    "000111000110100"  when "100000", -- add
-    "000000000110100"  when "100001", -- addu
-    "000001000110100"  when "100100", -- and
-    "000010100110100"  when "100111", -- nor
-    "000010000110100"  when "100110", -- xor
-    "000001100110100"  when "100101", -- or
-    "000011100110100"  when "101010", -- slt
-    "000011100110100"  when "101011", -- sltu (same as slt)
-    "000100100110100"  when "000000", -- sll
-    "000100000110000"  when "000010", -- srl
-    "000101000110100"  when "000011", -- sra
-    "000111100110100"  when "100010", -- sub
-    "000000100110100"  when "100011", -- subu
-    "100000000000110"  when "001000", -- jr
-    "000000000000000"  when others;
+--ALUControl--
+with opCode select
+     s_aluOp1 <= "1111" when "000000",
+                 "0000" when "001100", -- andi
+                 "0001" when "001101",
+                 "0010" when "001000" | "001001" | "101011" | "100011",
+                 "0100" when "001110",
+                 "0110" when "001010",
+                 "1010" when "011111",
+                 "1100" when "001111",
+                 "0011" when "000100" | "000101",
+                 "1111" when others;
 
-with i_opcode select o_Ctrl_Unt <=
-    s_RTYPE  	    when "000000", -- RTYPE
-    "001111000100100"  when "001000", -- addi
+with funct select
+     s_aluOp2 <= "0000" when "100100",
+                 "0001" when "100101",
+                 "0010" when "100000" | "100001",
+                 "0011" when "100010" | "100011",
+                 "0100" when "100110",
+                 "0101" when "100111",
+                 "0110" when "101010",
+                 "0111" when "000000",
+                 "1000" when "000010",
+                 "1001" when "000011",
+                 "1010" when "111111",
+                 "1011" when "110000",
+                 "1111" when others;
 
-    "000000000000001"  when "010100", -- halt
+       
+with opCode select
+     ALUOp <= s_aluOp2 when "000000",
+              s_aluOp1 when others;
 
-    "001000000100100"  when "001001", -- addiu
-    "001001000100000"  when "001100", -- andi
-    "001010000100000"  when "001110", -- xori
-    "001001100100000"  when "001101", -- ori
-    "001011100100100"  when "001010", -- slti
-    "001011100100100"  when "001011", -- sltiu (same implementation as slti)
-    "001011000100100"  when "001111", -- lui
-    "000101100001100"  when "000100", -- beq
-    "000110000001100"  when "000101", -- bne
-    "001000010100100"  when "100011", -- lw
-    "001000001000100"  when "101011", -- sw
-    "000000000000110"  when "000010", -- j
-    "010000000100110"  when "000011", -- jal
-    "000000000000000"  when others;
+--MemtoReg--
+with opCode select
+     MemtoReg <= '1' when "100011",
+                 '0' when others;
+
+--MemWrite--
+with opCode select
+     MemWrite <= '1' when "101011",
+                 '0' when others;
+
+--MemRead--
+with opCode select
+     MemRead <= '1' when "100011",
+                '0' when others;
+
+--RegWrite--
+with opCode select
+     s_rw1 <= '0' when "101011" | "000100" | "000101" | "000010",
+              '1' when others;
+
+with funct select
+     s_rw2 <= '0' when "001000",
+              '1' when others;
+
+with opCode select
+     RegWrite <= s_rw2 when "000000",
+                 s_rw1 when others;
+
+
+--RegDst--
+with opCode select
+     s_Rds1 <= "10" when "000011",
+                    "00" when others;
+
+with funct select
+     s_Rds2 <= "01" when "100000" | "100001" | "100100" | "110000" | "100111" | "100110" | "100101" | "101010" | "100010" | "111111" | "100011",
+               "01" when others;
+
+with opCode select
+     RegDst <= s_Rds2 when "000000" | "011111",
+               s_Rds1 when others;
+
+--Jump--
+with opCode select
+     s_j1 <= '1' when "000010" | "000011",
+             '0' when others;
+
+with funct select
+     s_j2 <= '1' when "001000",
+             '0' when others;
+
+with opCode select
+     Jump <= s_j2 when "000000",
+             s_j1 when others;
+
+--Branch--
+with opCode select
+     Branch <= '1' when "000100",
+               '1' when "000101",
+               '0' when others;
+--Shift--
+with opCode&funct select
+     Shift <= '1' when "000000000000",
+              '1' when "000000000010",
+              '1' when "000000000011",
+              '0' when others;
+
+--SignExtend--
+with funct select
+     s_se2 <= '0' when "001101" | "001100",
+     '1' when others;
+
+with opCode select
+     s_se1 <= '0' when "001101" | "001100",
+     '1' when others;
+
+
+with opCode select
+     SignExtend <= s_se2 when "000000",
+     s_se1 when others;
+
+--Overflow Enable--
+with funct select
+     s_ofe1 <= '0' when "100001" | "100100" | "100011",
+     '1' when others;
+
+with opCode select
+     s_ofe2 <= '0' when "001001",
+     '1' when others;
+
+with opCode select
+     OverflowEn <= s_ofe1 when "000000",
+     s_ofe2 when others;
+
+--Halt--
+with opCode select
+     Halt <= '1' when "010100",
+             '0' when others;
+
+--opSlt--
+with funct select
+     opSlt <= '1' when "101010",
+              '0' when others;
+
+--opJal--
+with opCode select
+     opJal <= '1' when "000011",
+              '0' when others;
+
+--opJr--
+with opCode & funct select
+     opJr <= '1' when "000000001000",
+             '0' when others;
+
+--opBne--
+with opCode select
+     opBne <= '1' when "000101",
+              '0' when others;
+
 
 end dataflow;
