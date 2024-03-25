f7a6da9bafe441494b8d672ef08aa1a61744de68
Ethan Griesman <egriesma@iastate.edu>
2024-03-25 02:50:33 -0500

    ALU work in progress

--- src/ALU/ALU.vhd
+++ src/ALU/ALU.vhd
@@ -1,3 +1,15 @@
+-- Ethan Griesman
+-- Department of Electrical and Computer Engineering
+-- Iowa State University
+-- Spring 2024
+--------------------------------------------------------------------------------------
+
+
+-- ALU.vhd
+--------------------------------------------------------------------------------------
+-- DESCRIPTION: This file contains an implementation of the ALU of the MIPS processor
+--------------------------------------------------------------------------------------
+
 library IEEE;
 use IEEE.std_logic_1164.all;
 
@@ -30,5 +42,60 @@ port(i_A      : in std_logic_vector(31 downto 0);
 end component;
 
 
+--OR
+component org2 is
+     port(i_A	: in std_logic;
+          i_B	: in std_logic;
+          o_F	: out std_logic);
+end component;
+     
+--AND
+component andg2 is
+     port(i_A	: in std_logic;
+          i_B	: in std_logic;
+          o_F	: out std_logic);
+end component;
 
+--XOR
+component xorg2 is
+     port(i_A          : in std_logic;
+          i_B          : in std_logic;
+          o_F          : out std_logic);
+end component;
+     
+--One's Complementor/NOT
+component onesComp is
+generic(n: positive);
+     port(
+          i_D: in std_logic_vector(n-1 downto 0);
+          o_O: out std_logic_vector(n-1 downto 0)
+          );
+end component;
+
+-----------
+--Signals--
+-----------
+
+signal s_and  : std_logic_vector(31 downto 0);
+signal s_or   : std_logic_vector(31 downto 0);
+signal s_add  : std_logic_vector(31 downto 0);
+signal s_sub  : std_logic_vector(31 downto 0);
+signal s_xor  : std_logic_vector(31 downto 0);
+signal s_nor  : std_logic_vector(31 downto 0);
+signal s_hold : std_logic_vector(31 downto 0);
+signal s_slt  : std_logic_vector(31 downto 0);
+signal s_sll  : std_logic_vector(31 downto 0);
+signal s_srl  : std_logic_vector(31 downto 0);
+signal s_sra  : std_logic_vector(31 downto 0);
+signal s_rep  : std_logic_vector(31 downto 0);
+signal s_not  : std_logic_vector(31 downto 0);
+signal s_lui  : std_logic_vector(31 downto 0);
+signal s_zero : std_logic_vector(31 downto 0);
+signal s_cm   : std_logic;
+signal s_co   : std_logic;
+signal s_of_detect: std_logic;
+
+
+     
+     
 end mixed;
