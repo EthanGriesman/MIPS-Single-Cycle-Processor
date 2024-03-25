57852067b928e62ea7e866f6c974708175378844
Ethan Griesman <egriesma@iastate.edu>
2024-03-25 15:55:25 -0500

    updated fetch testbench, attempting to fix issue in simulating

--- src/fetch/tb_fetch.vhd
+++ src/fetch/tb_fetch.vhd
@@ -49,6 +49,26 @@ begin
             oPCPlus4       => s_PCPlus4
         );
 
+    --This first process is to setup the clock for the test bench
+    P_CLK: process
+    begin
+      CLK <= '1';         -- clock starts at 1
+      wait for gCLK_HPER; -- after half a cycle
+       CLK <= '0';         -- clock becomes a 0 (negative edge)
+      wait for gCLK_HPER; -- after half a cycle, process begins evaluation again
+    end process;
+
+    P_RST: process
+    begin
+  	  reset <= '0';   
+      wait for gCLK_HPER/2;
+	  reset <= '1';
+      wait for gCLK_HPER*2;
+	  reset <= '0';
+	  wait;
+    end process;  
+
+
     -- Test Process
     process
     begin
@@ -65,17 +85,23 @@ begin
         s_Branch <= '0';
         s_ALUZero <= '0';
         s_Jump <= "00";
+        -- Expected Output: PC should increment by 4 due to sequential execution.
+        -- PCPlus4 should be the next instruction address.
 
         -- Test Case 2: Branch Taken
         wait for 10 ns;
         s_Branch <= '1';
         s_ALUZero <= '1';  -- Simulate branch condition met
         s_SignExtendImm <= "00000000000000000000000000001000";  -- Simulate branch offset
+        -- Expected Output: PC should be updated to PC + 4 + BranchOffset (if branch condition is met).
+        -- PCPlus4 should be the next instruction address.
 
         -- Test Case 3: Jump Execution
         wait for 10 ns;
         s_Jump <= "01";
         s_Addr <= "00000000000000000000001000";  -- Simulate jump address
+        -- Expected Output: PC should be updated to the specified jump address.
+        -- PCPlus4 should be the next instruction address.
 
         -- Test Case 4: Jump Register (JR) Execution
         wait for 10 ns;
