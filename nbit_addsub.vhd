8427f51395abdee3b442abdca503ba02c9e25cf1
Ethan Griesman <egriesma@iastate.edu>
2024-03-30 16:06:59 -0500

    .

--- nbit_addsub.vhd
+++ nbit_addsub.vhd
@@ -1,7 +1,20 @@
+-------------------------------------------------------------------------
+-- Ethan Griesman	
+-- Iowa State University
+-------------------------------------------------------------------------
+
+
+-- nbit_addsub.vhd
+-------------------------------------------------------------------------
+-- DESCRIPTION: This file contains implementation of an N-bit ripple-carry adder
+--
+-- NOTES:
+-- 3/28/24 by EG::Design Created
+-------------------------------------------------------------------------
 library IEEE;
 use IEEE.std_logic_1164.all;
 
-entity n_bit_adder_sub is 
+entity nbit_addsub is 
 	generic(N : integer := 32);
 	port(
 		 CLK 		: in std_logic;
@@ -12,9 +25,9 @@ entity n_bit_adder_sub is
 		 o_Cm     : out std_logic;
 		 o_C      : out std_logic
  	);
-end n_bit_adder_sub;
+end nbit_addsub;
 
-architecture structural of n_bit_adder_sub is
+architecture structural of nbit_addsub is
 
 	component mux2t1_N is
 		generic(N : integer := 16);
