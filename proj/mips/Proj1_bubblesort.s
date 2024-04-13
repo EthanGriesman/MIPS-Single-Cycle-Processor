bafa56c9cde90d346343b4311a851596d841c570
Ethan Griesman <egriesma@iastate.edu>
2024-04-13 17:08:43 -0500

    progress on bubble sort and other additional mips tests, error with other mips tests not running

--- proj/mips/Proj1_bubblesort.s
+++ proj/mips/Proj1_bubblesort.s
@@ -1,63 +1,71 @@
+# Ethan Griesman
+# Spring 2024
+# Department of Electrical and Computer Engineering
+# Iowa State University
+#
+# This MIPS assembly code sorts an array of integers using the Bubble Sort algorithm.
+# It iteratively compares and swaps adjacent elements if they are in the wrong order,
+# ensuring the largest elements bubble to the end of the array after each iteration.
+
 .data
-list:.word   5, 7, 9, 2, 3, 6, 11
-size: .word 7
+list: .word 5, 7, 9, 2, 3, 6, 11    # Array to sort
+size: .word 7                       # Number of elements in the array
 
 .text
 .globl main
 
 main:
-                                            # $t0, outer loop iterator (i)
-                                            # $t1, inner loop iterator (j)
-                                            # $s0, list base address
-                                            # $s1, size
-                                            # $t7, inner loop upper bound
-                                            # $t6, temporary
-                                            # $s2, word a
-                                            # $s3, word b
+    # Initialize registers
+    li $t0, 0                        # $t0, outer loop iterator (i)
+    la $s0, list                     # $s0, base address of the list
+    lw $s1, size                     # $s1, size of the list
+    li $t7, 0                        # $t7, inner loop upper bound initially 0
 
-    add     $t0,        $zero,  $zero
-    la      $s0,        list
-    lw      $s1,        size
-    addi    $t7,        $zero,  0
+    # Calculate the starting upper bound for the inner loop
+    lw $t7, size                     # Load the size into $t7
+    addi $t7, $t7, -1                # Decrement by 1 to account for 0-based index
 
 outer_loop:
-                                            # Check if we've walked the whole list
-    beq     $t0,        $s1,    exit
+    # Termination condition for the outer loop
+    beq $t0, $s1, exit               # If i equals array size, all elements are sorted
 
-# upper bound of inner loop = size - i - 1
-    sub     $t7,        $s1,    $t0
-    addi    $t7,        $t7,    -1
+    # Set upper bound for inner loop (size - i - 1)
+    sub $t7, $s1, $t0
+    addi $t7, $t7, -1
 
-# j = 0
-    add     $t1,        $zero,  $zero
+    # Reset inner loop iterator
+    li $t1, 0                        # $t1, inner loop iterator (j)
 
-# for (j < $t7)
 inner_loop:
+    # Termination condition for the inner loop
+    bge $t1, $t7, outer_loop_increment
 
-    beq     $t1,        $t7,    continue
+    # Calculate addresses for current and next elements
+    sll $t6, $t1, 2                  # Multiply index j by 4 to get word offset
+    add $t6, $s0, $t6                # Add offset to base address to get element address
 
+    # Load current and next elements
+    lw $s2, 0($t6)                   # Load element at index j into $s2
+    lw $s3, 4($t6)                   # Load element at index j+1 into $s3
 
-    sll     $t6,        $t1,    2           # Mult i by 4 to get correct address
-    add     $t6,        $s0,    $t6
+    # Compare and possibly swap elements
+    slt $t2, $s2, $s3                # Set $t2 to 1 if $s2 < $s3 (wrong order)
+    beq $t2, $zero, inner_loop_increment  # If $s2 is less than $s3, go to next iteration
 
-    lw      $s2,        0($t6)
-    lw      $s3,        4($t6)
+    # Swap elements
+    sw $s3, 0($t6)                   # Store $s3 at index j
+    sw $s2, 4($t6)                   # Store $s2 at index j+1
 
-# if $s3 > $s2, swap, else goto sorted
-    slt     $t2,        $s3,    $s2
-    beq     $t2,        $zero,  sorted
-    sw      $s3,        0($t6)
-    sw      $s2,        4($t6)
+inner_loop_increment:
+    # Increment inner loop iterator
+    addi $t1, $t1, 1
+    j inner_loop                     # Jump back to the start of inner loop
 
-sorted:
-                                            # j++
-    addi    $t1,        $t1,    1
-    j       inner_loop
-
-continue:
-                                            # i++
-    addi    $t0,        $t0,    1
-    j       outer_loop
+outer_loop_increment:
+    # Increment outer loop iterator
+    addi $t0, $t0, 1
+    j outer_loop                     # Jump back to the start of outer loop
 
 exit:
-    halt    
+    li $v0, 10                       # Load exit syscall
+    syscall                          # Exit the program
