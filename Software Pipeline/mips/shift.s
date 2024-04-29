0219824e533079a8237429e8048c04b03c7b403f
hpeacher <hpeacher@co2050-13.ece.iastate.edu>
2024-04-29 13:15:28 -0500

    added some several nops that were missing. currently working except any pseudoinstructions since nops cannot be inserted between instructions in a pseudoinstruction.

--- Software Pipeline/mips/shift.s
+++ Software Pipeline/mips/shift.s
@@ -1,4 +1,4 @@
--- Ethan Griesman
+# Ethan Griesman
 	srl $t5, $t5, 7
 	sra $t4, $t4, 9
 	sub $s2, $s2, $s3
--- Software Pipeline/mips/simpleTestAllInstruction.s
+++ Software Pipeline/mips/simpleTestAllInstruction.s
@@ -15,9 +15,7 @@ main:
 	NOP
 	NOP
 	addu $t2, $zero, $a3 	#$t2 = 0xfffffff0
-	
-	NOP
-	NOP
+
 	NOP
 	addiu $a3, $t3, -2 	#$a3 = 28
 	NOP
@@ -35,7 +33,7 @@ main:
 	andi  $t2, $t1, 0       #$t2 = 0x00000000
 	
 	# test on load words 
-	lui   $t4, 0x00001001  #$t4 = 0x10010000
+	lui   $t4, 0x1001  #$t4 = 0x10010000
 	NOP
 	NOP
 	NOP
@@ -45,12 +43,16 @@ main:
 	
 	#test nor, xor , xori 
 	
-	nor   $t5, $a0, $zero # $a0 = 0xfffffffe nor zero = 0x00000001 (only 0 & 0 has output 1) 
+	nor   $t5, $a0, $zero # $a0 = 0xfffffffe nor zero = 0x00000001 (only 0 & 0 has output 1)
+	NOP
+	NOP
+	NOP 
 	xor   $t6, $t1, $t5   # $t1 = 0x0000001e xor 0x00000001 = 0x0000001f
 	xori  $t2, $a1, 100   # 0x0000000a xor 0x00000064 = 0x0000006e
 	
 	or    $t3, $t1, $t5  # t3 = 0x000000lf 
 	ori    $t7, $a2, -1  # $t7 = 0xffffffff
+	NOP
 	
 	slt   $t1, $t2, $t1  # set $t1 to 0
 	slt   $t5, $t5, $a1  #set $t5 to 1
@@ -62,6 +64,9 @@ main:
 	srl   $t5, $a2, 2    # 20 / 4, $t5 = 5 
 	
 	addi  $s0, $0, -53 
+	NOP
+	NOP
+	NOP
 	sra   $t7, $s0, 2     # $t7 = -14 
 	
 	sub   $t0, $a2, $a1   # 20 -10 
@@ -70,34 +75,69 @@ main:
 	subu  $t3, $a1, $a2  
 	
 	beq   $t7, $s0, main 
+	NOP
+	NOP
+	NOP
 	addi  $s0, $zero, 0 
+	NOP
+	NOP
+	NOP
 	beq   $zero, $s0, test
+	NOP
+	NOP
+	NOP
 	addi  $s0, $zero, 100 
 	
 	test: 
 	addi $t7, $0, 100 
 	bne  $t1, $0, branch 
+	NOP
+	NOP
+	NOP
 	addi $t1, $0, 0
 	
 	branch: addi $t2, $0, 5  # i = 5 
 	j loop 
+	NOP
+	NOP
+	NOP
 	
 	loop: 
-	NOP
-	NOP 
 	addi $t7, $t7, 1   # 100 + 1 
 	addi $t2, $t2, -1  # i--
+	NOP
+	NOP
+	NOP
 	bne  $t2, $zero, loop
+	NOP
+	NOP
+	NOP
 	# $t0 = 105 
 	
 	jal branch2
+	NOP
+	NOP
+	NOP
 	add $t1, $0, 1	   # $t1 = 1 
+	NOP
+	NOP
+	NOP
 	beq $t2, $t1, exit #if ald execute branch2, go exit 
+	NOP
+	NOP
+	NOP
 	
 	branch2: 
 	addi $t2, $0, 1   # $t2 = 1
+	NOP
+	NOP
+	NOP
 	add $t0, $t2, $t7  # $t0 = 106
+	NOP
 	jr $ra
+	NOP
+	NOP
+	NOP
 	
 	
 	exit:
